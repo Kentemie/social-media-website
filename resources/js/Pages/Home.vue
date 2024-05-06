4a4ffe89c2afed57fe81de81f020313dcc305d23
Temirlan <kenzhakhmett@gmail.com>
2024-05-06 17:22:09 +0600

    fix: get rid of unnecessary routes and views

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps({
+
+});
+
+</script>
+
+<template>
+    <Head title="HoloNet" />
+
+    Test
+</template>
+
+<style>
+
+</style>
