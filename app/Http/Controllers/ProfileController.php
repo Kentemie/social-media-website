310eaa1985c9d1217c39554c77fae71f1e88ff3c
Temirlan <kenzhakhmett@gmail.com>
2024-05-08 00:49:54 +0600

    feat: implement updating the user profile avatar and cover images

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -22,7 +25,8 @@ class ProfileController extends Controller
         return Inertia::render('Profile/View', [
             'mustVerifyEmail' => $user instanceof MustVerifyEmail,
             'status' => session('status'),
-            'user' => $user,
+            'success' => session('success'),
+            'user' => new UserResource($user),
         ]);
     }
 
@@ -39,7 +43,7 @@ class ProfileController extends Controller
 
         $request->user()->save();
 
-        return Redirect::route('profile.edit');
+        return to_route('profile', $request->user())->with('success', 'Your profile has been updated.');
     }
 
     /**
@@ -61,5 +65,45 @@ class ProfileController extends Controller
         $request->session()->regenerateToken();
 
         return Redirect::to('/');
+    }
+
+    /**
+     * Update the user`s profile images
+     */
+    public function updateImage(Request $request): RedirectResponse
+    {
+        $user = $request->user();
+        $data = $request->validate([
+            'cover' => ['nullable', 'image'],
+            'avatar' => ['nullable', 'image'],
+        ]);
+
+        /** @var UploadedFile $cover */
+        $cover = $data['cover'] ?? null;
+
+        /** @var UploadedFile $avatar */
+        $avatar = $data['avatar'] ?? null;
+
+        $success = '';
+
+        if ($cover) {
+            if ($user->cover_path) {
+                Storage::disk('public')->delete($user->cover_path);
+            }
+            $path = $cover->store('user-' . $user->id, 'public');
+            $user->update(['cover_path' => $path]);
+            $success = 'Your cover image has been updated.';
+        }
+
+        if ($avatar) {
+            if ($user->avatar_path) {
+                Storage::disk('public')->delete($user->avatar_path);
+            }
+            $path = $avatar->store('user-' . $user->id, 'public');
+            $user->update(['avatar_path' => $path]);
+            $success = 'Your avatar image has been updated.';
+        }
+
+        return back()->with('success', $success);
     }
 }
