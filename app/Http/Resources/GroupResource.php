fa147b8c16a1b287fe1c5d9dc2f7c1fc99bf81f6
Temirlan <kenzhakhmett@gmail.com>
2024-05-17 14:48:27 +0500

    feat: implement the creation of groups

---
+++ app/Http/Resources/GroupResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class GroupResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'slug' => $this->slug,
+            'auto_approval' => $this->auto_approval,
+//            'cover_path' => $this->cover_path,
+//            'thumbnail_path' => $this->thumbnail_path,
+            'user_id' => $this->user_id,
+//            'deleted_by' => $this->deleted_by,
+//            'deleted_at' => $this->deleted_at,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
